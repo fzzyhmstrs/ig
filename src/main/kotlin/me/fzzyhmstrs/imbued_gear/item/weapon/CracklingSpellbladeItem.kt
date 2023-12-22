27c8458c51c5a2927a63dab0aeb5f7e5179e584a
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-12-22 10:45:13 -0500

    Update CracklingSpellbladeItem.kt

--- src/main/kotlin/me/fzzyhmstrs/imbued_gear/item/weapon/CracklingSpellbladeItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/imbued_gear/item/weapon/CracklingSpellbladeItem.kt
@@ -1,6 +1,7 @@
 package me.fzzyhmstrs.imbued_gear.item.weapon
 
 import me.fzzyhmstrs.amethyst_core.item_util.DefaultAugmentSwordItem
+import me.fzzyhmstrs.amethyst_core.registry.RegisterTag
 import me.fzzyhmstrs.fzzy_core.item_util.FlavorHelper
 import me.fzzyhmstrs.fzzy_core.modifier_util.ModifierHelperType
 import me.fzzyhmstrs.imbued_gear.IG
@@ -14,6 +15,10 @@ import net.minecraft.world.World
 
 class CracklingSpellbladeItem(settings: Settings): DefaultAugmentSwordItem(IgConfig.materials.tools.crackling.compatMaterial(),4,-2.9f,settings) {
     override val fallbackId: Identifier = IG.identity("bankai")
+
+    override fun canAcceptAugment(augment: ScepterAugment): Boolean{
+        return augment.isIn(RegisterTag.LIGHTNING_AUGMENTS)
+    }
 
     override fun canBeModifiedBy(type: ModifierHelperType<*>): Boolean {
         return true
